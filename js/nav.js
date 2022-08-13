50867a908915a29d92a91f3a96fa804e253afe86
JuanCelis25 <104436922+JuanCelis25@users.noreply.github.com>
2022-08-12 21:03:44 -0500

    Finish

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+'use strick'
+const icono = document.querySelector('.burg')
+
+
+const menu = document.querySelector('.menu')
+
+icono.addEventListener("click", ()=>{
+      menu.classList.toggle("active")
+})
